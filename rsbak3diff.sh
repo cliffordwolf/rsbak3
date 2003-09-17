e3e9d932e2ffc43414bba71a5616ccb9be9e370c
clifford <clifford@787cde76-47d9-0310-a09f-da94ad1acf18>
2003-09-17 09:46:15 +0000

    Added disk-usage summary to rsbak3diff.sh.
    git-svn-id: http://svn.linbit.com/rsbak3/trunk@7 787cde76-47d9-0310-a09f-da94ad1acf18

--- rsbak3diff.sh
+++ rsbak3diff.sh
@@ -10,16 +10,40 @@ diff -u \
 	<( find $2 -type f -printf '%i\t%k\t%P\n' | sort +2 ) | \
 perl -e '
 	while (<>) {
-		@_ = split /\t/;
+		chomp; @_ = split /\t/;
 		next if /^(---|\+\+\+)/ || $_[2] eq "";
 		$a{$_[2]} = $f{$_[2]} = $_[1] if /^\+/;
 		$d{$_[2]} = $f{$_[2]} = $_[1] if /^-/;
 	}
 	$s = 0;
 	foreach (sort keys %f) {
-		printf("- %9d %s", $d{$_}, $_), $s -= $d{$_} if !defined $a{$_} &&  defined $d{$_};
-		printf("+ %9d %s", $a{$_}, $_), $s += $a{$_} if  defined $a{$_} && !defined $d{$_};
-		printf("! %9d %s", $a{$_}, $_), $s = $s - $d{$_} + $a{$_} if  defined $a{$_} &&  defined $d{$_};
+		if ( !defined $a{$_} && defined $d{$_} ) {
+			printf("- %9d %s\n", $d{$_}, $_);
+			$s -= $d{$_};
+		}
+		if ( defined $a{$_} && !defined $d{$_} ) {
+			printf("+ %9d %s\n", $a{$_}, $_);
+			$s += $a{$_};
+		}
+		if ( defined $a{$_} && defined $d{$_} ) {
+			printf("! %9d %s\n", $a{$_}, $_);
+			$s = $s - $d{$_} + $a{$_};
+		}
+		$x = $_;
+		while ( $x ne "" ) {
+			$s{$x} = defined $s{$x} ? $s{$x} + $f{$_} : $f{$_};
+			$x =~ s/\/?[^\/]*$//;
+		}
+	}
+	print "\n";
+	foreach (keys %s) {
+		$u{sprintf "%011d %s", 100000000 - $s{$_}, $_} =
+			sprintf("* %9s %s", $s{$_}, $_);
+	}
+	$c=50;
+	foreach (sort keys %u) {
+		last if $c-- <= 0;
+		print "$u{$_}\n";
 	}
 	print "\nDifference: $s\n";
 '
